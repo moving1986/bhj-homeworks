99e1bdadd6bf9a95b3a074845c0ccabcf0e08be6
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2024-10-29 14:42:53 +0700

    complite dropdown task

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,23 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const dropValue = dropdown.querySelector('.dropdown__value');
+    const downList = dropdown.querySelector('.dropdown__list');
+    const links = dropdown.querySelectorAll('.dropdown__link');
+
+    dropValue.addEventListener('click', () => {
+        downList.classList.toggle('dropdown__list_active')
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            dropValue.textContent = link.textContent;
+            downList.classList.remove('dropdown__list_active');
+        })
+
+
+    });
+});
+
+
